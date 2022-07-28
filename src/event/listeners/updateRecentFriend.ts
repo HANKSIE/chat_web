a2768f9813bcf7e1237592da5b6a2ae7d35c87fc
HANKSIE <32055793+HANKSIE@users.noreply.github.com>
2022-07-28 08:07:34 +0800

    add unread handle when receive message

--- src/event/listeners/updateRecentFriend.ts
+++ src/event/listeners/updateRecentFriend.ts
@@ -1,15 +1,26 @@
 import EventType from "@/config/events";
+import useAuthStore from "@/stores/auth";
+import useChatroomStore from "@/stores/chatroom";
 import useRecentContactFriendStore from "@/stores/recentContactFriend";
 import Listener from "@/types/listener";
-import RecentContactFriendData from "@/types/responses/socialite/friend/recentContactCursorPaginate";
+import Message from "@/types/message";
 
 const recentContactFriendStore = useRecentContactFriendStore();
 
 const listener: Listener = {
   event: EventType.RECEIVE_GROUP_MESSAGE,
-  handle: (data: RecentContactFriendData) => {
-    if (data.message.group!.is_one_to_one) {
-      recentContactFriendStore.update(data);
+  handle: (message: Message) => {
+    if (message.group!.is_one_to_one) {
+      const unread = recentContactFriendStore.data.find(
+        (data) => data.message.group_id === message.group_id
+      )!.unread;
+      const isCurrentChatroom =
+        useChatroomStore().unit?.group_id === message.group_id;
+      const isMe = message.user.id === useAuthStore().user?.id;
+      recentContactFriendStore.update({
+        message,
+        unread: isCurrentChatroom || isMe ? 0 : unread + 1,
+      });
     }
   },
 };
